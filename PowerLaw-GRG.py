b1a983e5ac92cebd66e3e5ccc88f086f69d3a695
LourensT <touwenlourens@gmail.com>
2021-08-12 12:20:46 +0200

    first results

--- PowerLaw-GRG.py
+++ PowerLaw-GRG.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-n = 10000
+n = 1000
 tau = 3.5
 a = 1
 
@@ -23,7 +23,30 @@ def sampleWeight(size):
 
     return weights
 
-graph = GRG(sampleWeight(n))
+
+weights = sampleWeight(n)
+graph = GRG(weights)
 distr = graph.DegreeDistribrution()
-plt.loglog(distr.keys(), distr.values(), color='blue')
+
+# calculate tail distribution of weights
+weights.sort()
+total = sum(weights)
+cumulative = [sum(weights[n::])/total for n in range(len(weights))]
+prev = weights[0]
+index = [prev]
+freq = [cumulative[0]]
+for i in range(1, len(weights)):
+    if not (weights[i] == prev):
+        prev = weights[i]
+        index.append(weights[i])
+        freq.append(cumulative[i])
+
+# plot weight distribution
+plt.scatter(x=index, y=freq, color='blue')
+plt.title("Tail Distribution of vertex weights, tau=3.5, alpha=1")
+plt.show()
+
+# plot degree distribution
+plt.scatter(x=distr.keys(), y=distr.values(), color='red')
+plt.title("Tail Distribution of degrees, tau=3.5, alpha=1")
 plt.show()
---
+++ README.md
@@ -0,0 +1,3 @@
+# GRG with Powerlaw weights example
+
+inspect the degree distribution of a Generalized Random Graph. Implementation of GRG with Powerlaw distributed weights.
