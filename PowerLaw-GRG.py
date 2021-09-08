eb2725b86676f3d0d62a945ced0b348d5cadc6ab
LourensT <touwenlourens@gmail.com>
2021-09-08 11:56:18 +0200

    add plots in tikz format

--- PowerLaw-GRG.py
+++ PowerLaw-GRG.py
@@ -1,5 +1,6 @@
 from GRG import GRG
-from scipy import stats
+from TikzPlotsFromPython.GenerateTikz import GenerateTikz
+import os
 
 import numpy as np
 
@@ -23,6 +24,8 @@ def sampleWeight(size):
 
     return weights
 
+BASE_FP = os.getcwd() + "\\Plots\\"
+print(BASE_FP)
 
 weights = sampleWeight(n)
 graph = GRG(weights)
@@ -46,24 +49,36 @@ plt.title("Tail Distribution of vertex weights, tau=3.5, alpha=1")
 plt.show()
 
 
+
+# get degree distributions
 distr = graph.DegreeDistribrution(tail=True)
-distrBiased = graph.SizeBiasedDegreeDistribution(tail=True)
-friendBiased = graph.RandomFriendDegreeDistribution(tail=True)
+distrSizeBiased = graph.SizeBiasedDegreeDistribution(tail=True)
+distrfriendBiased = graph.RandomFriendDegreeDistribution(tail=True)
+
+# tikz object
+plot = GenerateTikz(os.path.join(BASE_FP, "degree_distribution.tikz"), documentation=f"Degree distributions of GRG (n={n}) with Powerlaw (tau=={tau}) distributed weights, following #6.2.21 from Hofstad RGCN Vol1. ")
+maximum_degrees = max(max(max(distr.keys()), max(distrSizeBiased.keys())), max(distrfriendBiased.keys()))
+plot.setConfiguration(0, maximum_degrees, 0, 1, False, False, xlabel="Degrees", ylabel="$P(X > x)$")
+
 # plot degree distribution
 plt.scatter(x=distr.keys(), y=distr.values(), color='red')
-print(max(distr.keys()))
-plt.scatter(x=distrBiased.keys(), y=distrBiased.values(), color='green')
-print(max(distrBiased.keys()))
-plt.scatter(x=friendBiased.keys(), y=friendBiased.values(), color='blue')
-print(max(friendBiased.keys()))
-plt.legend(["Normal Degree Distribution", "size Biased Degree Distribution", "Friend"])
+plot.addSeries(distr, "Degree distribution")
+plt.scatter(x=distrSizeBiased.keys(), y=distrSizeBiased.values(), color='green')
+plot.addSeries(distrSizeBiased, "Size biased degree distribution")
+plt.scatter(x=distrfriendBiased.keys(), y=distrfriendBiased.values(), color='blue')
+plot.addSeries(distrfriendBiased, "Random friend degree distribution")
+plt.legend(["Normal Degree Distribution", "size Biased Degree Distribution", "Random friend degree distribution"])
 plt.title("Tail Distributions of degrees, tau=3.5, alpha=1")
 plt.show()
 
 print("Size of Giant Component", graph.getSizeOfGiantComponent())
 
 typicalDist = graph.typicalDistanceDistribution()
+
+plot_distance = GenerateTikz(os.path.join(BASE_FP, "typical_distance.tikz"), documentation=f"Distribution of typical distance of GRG (n={n}) with Powerlaw (tau=={tau}) distributed weights, following #6.2.21 from Hofstad RGCN Vol1.")
+plot_distance.setConfiguration(0, max(typicalDist.keys()), 0, max(typicalDist.values()), False, False, xlabel="Typical Distance")
 # plot distance distribution
 plt.scatter(x=typicalDist.keys(), y=typicalDist.values(), color='red')
+plot_distance.addSeries(typicalDist, "Typical Distance")
 plt.title("Typical distance distribution, tau=3.5, alpha=1")
 plt.show()
